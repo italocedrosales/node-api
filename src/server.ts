7d46e00c58736811c17d267ee8eeda329e693c59
Italo Cedro <italo.cedro.sales@gmail.com>
2022-11-19 12:20:12 -0300

    feat: add iniciando servidor

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import * as dotenv from 'dotenv';
+import express from 'express';
+dotenv.config();
+
+const port = process.env.PORT || 3333;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
